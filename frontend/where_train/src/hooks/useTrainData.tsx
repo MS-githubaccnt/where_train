2394dca836a33db056d1f1713170d7e8b3fb0bf7
MS-githubaccnt <kindle.maheep@gmail.com>
2025-03-15 18:49:56 +0530

    feat:made pointers move

--- frontend/where_train/src/hooks/useTrainData.tsx
+++ frontend/where_train/src/hooks/useTrainData.tsx
@@ -1,58 +1,27 @@
-import { useEffect, useState } from "react";
-import getTrainData from "../services/trainStatus";
-const convertToSeconds=(timeStr:string):number=>{
-    const [hours,minutes]=timeStr.split(":").map(Number);
-    return hours*3600+minutes*60;
-};
-const useTrainData=()=>{
-    //coord stat stationnames traininfo
-    const [trainInfo,setTrainInfo]=useState(null);
-    const [staData,setStaData]=useState<number[]>([]);
-    const [coordData,setCoordsData]=useState<number[][]>([]);
-    const [stationNames,setStationNames]=useState<string[]>([]);
-    useEffect(()=>{
-    const fetchData=async()=>{
-    try{
-      const all_stations:string[]=[];
-      const all_stas=[];
-      const all_lat_lngs: number[][]=[];
-      const response=await getTrainData();
-      console.log(response);
-      const data=response;
-      console.log(`sending data${data}`);
-     // onTrainInfo(response);
-      let current_stn=data.current_station_name;
-      //previous stations
-        data.previous_stations.map((station:any)=>{
-          all_stations.push(station.station_name);
-          all_stas.push(convertToSeconds(station.sta));
-          all_lat_lngs.push([station.station_lat,station.station_lng]);
-        })
-        //current station
-        all_stations.push(current_stn);
-        all_stas.push(data.cur_stn_sta);
-        //upcoming station
-        data.upcoming_stations.map((station:any)=>{
-          all_stations.push(station.station_name);
-          all_stas.push(convertToSeconds(station.sta));
-          all_lat_lngs.push([station.station_lat,station.station_lng]);
-        })
-       // onStationNames(all_stations);
-        //onStaData(all_stas);
-        console.log(response);
-        //onCoordData(all_lat_lngs);
-        setTrainInfo(response);
-        setCoordsData(all_lat_lngs);
-        setStaData(all_stas);
-        setStationNames(all_stations);
+// import { useEffect, useState } from "react";
+// import getTrainData from "../services/trainStatus";
+// const useTrainData=()=>{
+//     //coord stat stationnames traininfo
+//     // const [trainInfo,setTrainInfo]=useState(null);
+//     // const [staData,setStaData]=useState<number[]>([]);
+//     // const [coordData,setCoordsData]=useState<number[][]>([]);
+//     // const [stationNames,setStationNames]=useState<string[]>([]);
+//     useEffect(()=>{
+//     const fetchData=async()=>{
+//     try{
+//       // const all_stations:string[]=[];
+//       // const all_stas=[];
+//       // const all_lat_lngs: number[][]=[];
+//       const response=await getTrainData();
+//       console.log(response);
 
-    }catch(error){
-        console.error(error);
-    }
-}
-fetchData();
-},[]);
-return {trainInfo,staData,stationNames,coordData};
-}
+//     }catch(error){
+//         console.error(error);
+//     }
+// }
+// fetchData();
+// },[]);
+// return {"hi":"bye"};
+// }
 
-export default useTrainData;
+// export default useTrainData;
--- frontend/where_train/src/pages/MapPage.tsx
+++ frontend/where_train/src/pages/MapPage.tsx
@@ -2,25 +2,10 @@ import { Circle, MapContainer, Polyline, TileLayer } from "react-leaflet";
 import SearchBar from "../components/SearchBar";
 import { useState, useEffect } from "react";
 import TrainStatusCard from "../components/TrainStatusCard";
-import useTrainData from "../hooks/useTrainData";
-function upperBound(arr, target) {
-  let left = 0, right = arr.length;
-  while (left < right) {
-      let mid = Math.floor((left + right) / 2);
-      if (arr[mid] > target) right = mid;  // Move left if arr[mid] is greater
-      else left = mid + 1;  // Move right otherwise
-  }
-  return left;
-}
-export default function MapPage() {
-  const [staData,setStaData]=useState<number[]>([]);
-  const [coordData,setCoordData]=useState<number[][]>([]);
-  const [currCoords,setCurrCoords]=useState<number[]>([]);
-  const [passedStations,setPassedStations]=useState(0);
-  const [stationNames,setStationNames]=useState<string[]>([]);
-  const [trainInfo,setTrainInfo]=useState<any>(null);
-  const [currentTime,setCurrentTime]=useState<number>(0);
+import getTrainData from "../services/trainStatus";
+export default  function MapPage() {
 
+const [currentCoords,setCurrentCoords]=useState<number[][]>([]); 
   const handleCoordData=(data:number[][])=>{}//(data);
  const handleStaData=(data:string[])=>{//setStaData(data);
 }
@@ -31,45 +16,34 @@ export default function MapPage() {
     //setTrainInfo(data);
   };
   useEffect(()=>{
-    const response=useTrainData();
-    setTrainInfo(response.trainInfo);
-    setStaData(response.staData);
-    setStationNames(response.stationNames);
-    setCoordData(response.coordData);
+    async function doit(){
+      const response=await getTrainData();
+      console.log(response)
+      function make_move(response:any[]){
+    console.log("call");
     const  now=new Date();
-    const currTime=now.getHours()*3600+now.getMinutes()*60;
-    setCurrentTime(currTime);
+    const currTime=now.getHours()*3600+now.getMinutes()*60+now.getSeconds();
+    let current_coords:number[][]=[]
+    for(let i=0;i<response.length;i++){
+      let train=response[i];
+      console.log(train);
+      let coord=[0,0]
+      if(train.next_sta==train.last_sta){coord=train.next_lat_lng;}
+     else{ coord=[train.last_lat_lng[0]+(train.next_lat_lng[0]-train.last_lat_lng[0])*(train.next_sta-currTime)/(train.next_sta-train.last_sta),train.last_lat_lng[1]+(train.next_lat_lng[1]-train.last_lat_lng[1])*(train.next_sta-currTime)/(train.next_sta-train.last_sta)]}
+     console.log(coord);
+     current_coords.push(coord);
+    }
+    setCurrentCoords(current_coords);}
+    function make_it_move(){
+      make_move(response);
+    }
+    let interval=setInterval(make_it_move,500);
+    return()=>clearInterval(interval);
+  }
+  doit();
+  
   },[]);
 
-  useEffect(()=>{
-    if (coordData.length===0||staData.length===0) return;
-    
-    let stationIndex=passedStations;
-    let interval:NodeJS.Timeout;
-    const moveTrain=()=>{
-      if(stationIndex>=etaData.length-1) return;
-      const etaSeconds=convertToSeconds(etaData[stationIndex]);
-      const nextEtaSeconds=convertToSeconds(etaData[stationIndex+1]);
-      if(timeOfRequest>=nextEtaSeconds){
-        stationIndex++;
-        setPassedStations(stationIndex);
-      }else{
-        const elapsed=timeOfRequest-etaSeconds;
-        const totalTravelTime=nextEtaSeconds-etaSeconds;
-        const startCoord=coordData[stationIndex];
-        const endCoord=coordData[stationIndex+1];
-        if (!startCoord||!endCoord) return;
-        const progress=Math.min(elapsed/totalTravelTime,1);
-        setCurrCoords([
-          startCoord[0]+progress*(endCoord[0]-startCoord[0]),
-          startCoord[1]+progress*(endCoord[1]-startCoord[1])
-        ]);
-        setTimeOfRequest(timeOfRequest+1);
-      }
-    };
-    interval=setInterval(moveTrain,1000);
-    return()=>clearInterval(interval);
-  },[timeOfRequest,coordData,etaData,passedStations]);
   return (
     <div className="relative h-screen w-full">
       <div className="absolute top-4 left-1/2 transform -translate-x-1/2 z-[5000]">
@@ -77,17 +51,18 @@ export default function MapPage() {
       </div>
       <MapContainer center={[28.7, 77.1]} zoom={5} scrollWheelZoom={false} className="h-full w-full">
         <TileLayer attribution='&copy; OpenStreetMap contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        {coordData.length>0&&<Polyline positions={coordData} color="blue"/>}
+        {currentCoords.length>0&&
+          currentCoords.map((coord,index)=>(
+            <Circle key={index} center={coord} radius={500} color='blue'></Circle>
+          ))
+        /* {coordData.length>0&&<Polyline positions={coordData} color="blue"/>}
         {prevCoordData.length>0&&<Polyline positions={prevCoordData} color="blue" dashArray="10,5"/>}
-        {currCoords.length>0&&<Circle center={currCoords} radius={2800}/>}
+        {currCoords.length>0&&<Circle center={currCoords} radius={2800}/>} */}
       </MapContainer>
-      <div className="absolute top-10 left-10 z-[5001]">
+      {/* <div className="absolute top-10 left-10 z-[5001]">
         {trainInfo&&<TrainStatusCard data={trainInfo}/>}
-      </div>
+      </div> */}
     </div>
   );
-}
-function startUpFunction() {
-  throw new Error("Function not implemented.");
 }
 
